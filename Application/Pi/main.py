40cc7c0713d7b54f06718727c0c9c3e12bd34a8c
AELIENUS <elia.herzog@web.de>
2020-04-22 12:27:37 +0200

    Full Restore

--- Application/Pi/main.py
+++ Application/Pi/main.py
@@ -1,7 +1,5 @@
 import tkinter as tk
 import pygubu
-import gpiozero
-import time
 
 class Application:
     def __init__(self, master):
@@ -10,32 +8,12 @@ class Application:
         self.builder = builder = pygubu.Builder()
 
         #2: Load an ui file
-        builder.add_from_file("S:\OneTouch\Application\Pi\PiUI.ui") #D:\OneTouch\OneTouch\Application\Pi\ unter Surface
-                                        #S:\OneTouch\Application\Pi\
-                                        #für Ausführung unter Pi nur PiUI.ui
+        builder.add_from_file("PiUI.ui") #D:\OneTouch\OneTouch\Application\Pi\
 
         #3: Create the widget using a master as parent
         self.mainwindow = builder.get_object("mainwindow", master)
 
         builder.connect_callbacks(self)
-
-        #Connecting to Pumps :D
-        # Bus 001 Device 006: ID 2109:2813 VIA Labs, Inc. 
-        # Bus 001 Device 005: ID 2109:2813 VIA Labs, Inc. 
-        # Bus 002 Device 007: ID 2109:0813 VIA Labs, Inc. 
-        # Bus 002 Device 006: ID 2109:0813 VIA Labs, Inc. 
-        # das sind die neuen Geräte nach Anstecken des Hubs an den Pi 
-        # leider kann er die Pumpe nicht finden D:
-        Combobox_1
-
-
-    def on_Button_1_click(self):
-        pump = GPIODevice(27)
-        pump.On()
-        sleep(2)
-        pump.Off()
-        print("something should happen")
-
 
 if __name__ == '__main__':
     root = tk.Tk()
