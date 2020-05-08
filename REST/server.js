6a045b91efa08a27763ee25d5d421b5891708e7b
TheLordXII <56317585+TheLordXII@users.noreply.github.com>
2020-05-08 18:10:11 +0200

    Update server.js
    added some Routes

--- REST/server.js
+++ REST/server.js
@@ -79,6 +79,43 @@ router.route('/drinks')
 //             //create request object
 //     });
 
+router.route('/drink/bestdrinkers')
+    //get Drinkers Tier List
+    .get(function(req, res){
+        console.log('/drinks/bestdrinkers geroutet')
+        //sql connection
+        sql.connect(config, function(err) {
+            if(err) console.log(err);
+        
+        //building request
+        var request = new sql.Request();
+        request.query('SELECT Benutzername,Drinks_Taken FROM [dbo].[User] ORDER BY Drinks_Taken DESC', function(err, result){
+            if (err) console.log(err)
+            //send recordset as result
+            res.json({ Data: result.recordset});
+        });
+    });
+});
+
+router.route('/drink/bestdrinks')
+    //get Best Drinks Tier List
+    .get(function(req, res){
+        console.log('/drinks/bestdrinks geroutet')
+        //sql connection
+        sql.connect(config, function(err) {
+            if(err) console.log(err);
+        
+        //building request
+        var request = new sql.Request();
+        request.query('SELECT [Name],[Times_Taken] FROM [dbo].[Drink] ORDER BY Times_Taken DESC', function(err, result){
+            if (err) console.log(err)
+            //send recordset as result
+            //console.log(result.recordset);
+            res.json({ Data: result.recordset});
+        });
+    });
+});
+
 router.route('/drinks/:value')
     //get one drink from the database
     .get(function(req, res) {
@@ -94,6 +131,7 @@ router.route('/drinks/:value')
         request.query('SELECT * FROM Drink WHERE DrinkID = @drinkid', function(err, result) {
             if (err) console.log(err)
             //send recordset as the result
+            console.log(result.recordset);
             res.json({ Data: result.recordset});
             
         });
@@ -114,6 +152,7 @@ router.route('/takedrink/drinkid=:did&userid=:uid')
         //input the url-value as parameter into the sql-statement
         request.input('did', sql.Int , req.params.did);
         request.input('uid', sql.Int, req.params.uid);
+
         request.query('UPDATE Drink SET Times_Taken = Times_Taken + 1 WHERE DrinkID = @did; UPDATE [dbo].[User] SET Drinks_Taken = Drinks_Taken + 1 WHERE UserID = @uid;', function(err, result) {
             if (err) console.log(err)
             //send message as the result
@@ -123,8 +162,47 @@ router.route('/takedrink/drinkid=:did&userid=:uid')
         
     });
 
+router.route('/ingredientlist/:value')
+    //get ingredientlist from the database
+    .get(function(req, res) {
+        console.log('/ingredientlist geroutet')
+        //sql connection
+        sql.connect(config, function (err) {
+            if (err) console.log(err);
+        
+        //bulding request
+        var request = new sql.Request();
+        //input the url-value as parameter into the sql-statement
+        request.input('drinkid', sql.Int , req.params.value);
+        request.query('SELECT Ingredient.Name, DrinkToIngredient.How_Much FROM DrinkToIngredient RIGHT JOIN Ingredient ON Ingredient.IngredientID = DrinkToIngredient.IngredientID WHERE DrinkID = @drinkid', function(err, result) {
+            if (err) console.log(err)
+            //send recordset as the result
+            console.log(result.recordset);
+            res.json({ Data: result.recordset});
+            
+        });
+    });
+    });
+
+router.route('/user')
+    .get(function(req, res){
+        //gets all users from Database
+        console.log('/user geroutet');
+        sql.connect(config, function(err){
+            if (err) console.log(err);
+
+        var request = new sql.Request();
+        request.query('SELECT * FROM [dbo].[User]', function(err, result){
+            if(err) console.log(err)
+
+            res.json({ Data: result.recordset});
+        });
+        });
+    });
+
 router.route('/user/login/username=:user&password=:pwd')
     .get(function(req, res) {
+        //checks if passwordcredentials are valid
         console.log('/user/login geroutet');
         //sql connection
         sql.connect(config, function (err) {
@@ -147,4 +225,49 @@ router.route('/user/login/username=:user&password=:pwd')
             }
         });
     });
+
+router.route('/newuser/username=:user&password=:pwd&name=:name&vorname=:vorname&gebdate=:date')
+    .post(function(req, res, next){
+        //puts a new user in the Database
+        console.log('/newuser geroutet');
+        //sql connection
+        sql.connect(config, function(err){
+            if(err) console.log(err);
+
+        //building request
+        var request = new sql.Request();
+        request.input('username', sql.NVarChar, req.params.user);
+        request.input('password', sql.NVarChar, req.params.pwd);
+        request.input('name', sql.NVarChar, req.params.name);
+        request.input('vorname', sql.NVarChar, req.params.vorname);
+        request.input('gebDate', sql.Date, req.params.date);
+
+        request.query('INSERT INTO [dbo].[User] ([Name],[Vorname],[Benutzername],[GebDatum],[Is_Admin],[Drinks_Taken],[Password]) VALUES (@name,@vorname,@username,@gebDate,0,0,@password)', function(err, result) {
+            if(err) console.log(err);
+
+        res.json({message: 'Successful'});
+        });
+        });
+
+    });
+
+router.route('/user/deleteuser/:value')
+    .delete(function(req, res){
+        //deletes a user from the Database
+        console.log('/user/deleteuser geroutet');
+        //sql connection
+        sql.connect(config, function(err){
+            if(err) console.log(err);
+
+        //building request
+        var request = new sql.Request();
+        request.input('uid', sql.Int, req.params.value);
+        request.query('DELETE FROM [dbo].[User] WHERE UserID = @uid', function(res, err){
+            if (err) console.log(err);
+
+        res.json({message: 'User deleted'});
+        });
+        });
+    });
+
 });
